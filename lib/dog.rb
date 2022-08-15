6bc8fb43d12148f5c8be49ddb98ff59196543885
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-08-15 09:36:06 +0300

    Assignment Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
-class Dog
+require 'pry'
 
+class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    
+    def name
+        @this_dogs_name
+    end
 end
+
+# puts Dog.inspect
+
+lassie = Dog.new
+# lassie.name=  "Lassie"
+
+lassie.name #=> "Lassie"
+# binding.pry
+
+
+
